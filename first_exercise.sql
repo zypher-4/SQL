ce1bcfe0df8d8bf70e139c2b2a51ed65bcb207a2
Wasil <wasil.ahmad4@gmail.com>
2024-08-19 14:47:58 +0400

    first-commit

---
+++ first_exercise.sql
@@ -0,0 +1,7 @@
+-- SELECT * from employees
+-- SELECT * FROM "public"."employees"
+-- select count( * ) from departments 
+-- SELECT (count( * ) - 1)  as "Number of Raises" FROM salaries WHERE emp_no = 10001
+-- SELECT title as "Title" FROM titles where emp_no = 10006
+-- select emp_no as "Employee #", birth_date as "Birthday", first_name as "First Name" from employees;
+-- SELECT * from employees;
