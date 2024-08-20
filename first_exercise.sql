5a2163e9767dca0a10face0448b70e8348fb9f38
Wasil <wasil.ahmad4@gmail.com>
2024-08-20 16:50:11 +0400

    Removing duplicates

--- first_exercise.sql
+++
@@ -1,8 +0,0 @@
--- SELECT * from employees
--- SELECT * FROM "public"."employees"
--- select count( * ) from departments 
--- SELECT (count( * ) - 1)  as "Number of Raises" FROM salaries WHERE emp_no = 10001
--- SELECT title as "Title" FROM titles where emp_no = 10006
--- select emp_no as "Employee #", birth_date as "Birthday", first_name as "First Name" from employees;
-SELECT * FROM employees;
--- SELECT concat(emp_no, ' is a ', title) as "Employee Title" FROM titles
